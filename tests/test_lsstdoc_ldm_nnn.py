3754d69bc1a96d81bde58e3e25a603288608aa23
Jonathan Sick <jsick@lsst.org>
2017-09-11 14:36:13 -0700

    Add LsstDoc plain format attributes
    LsstDoc.plain_title
    LsstDoc.plain_short_title
    LsstDoc.plain_authors
    LsstDoc.plain_abstract
    
    These attributes are useful for creating unformatted metadata (like html
    header tags, or database fields).

--- tests/test_lsstdoc_ldm_nnn.py
+++ tests/test_lsstdoc_ldm_nnn.py
@@ -13,9 +13,15 @@ HTML_TITLE = "Title of document\n"
 
 HTML_SHORT_TITLE = "Short title\n"
 
+PLAIN_TITLE = "Title of document\n"
+
+PLAIN_SHORT_TITLE = "Short title\n"
+
 AUTHORS = ['A. Author', 'B. Author', 'C. Author']
 
 HTML_AUTHORS = ['A. Author', 'B. Author', 'C. Author']
+
+PLAIN_AUTHORS = ['A. Author', 'B. Author', 'C. Author']
 
 ABSTRACT = (
     "This document demonstrates how to use the LSST \\LaTeX\\ "
@@ -31,6 +37,13 @@ HTML_ABSTRACT = (
     'path.</p>\n'
 )
 
+PLAIN_ABSTRACT = (
+    'This document demonstrates how to use the LSST LaTeX\xa0class files '
+    'to make Data Management documents. Build this document in the normal '
+    'way, making sure that the class file is available in the LaTeX\xa0load '
+    'path.\n'
+)
+
 IS_DRAFT = True
 
 HANDLE = 'LDM-nnn'
@@ -44,10 +57,14 @@ ATTRIBUTES = [
     ('short_title', SHORT_TITLE),
     ('html_title', HTML_TITLE),
     ('html_short_title', HTML_SHORT_TITLE),
+    ('plain_title', PLAIN_TITLE),
+    ('plain_short_title', PLAIN_SHORT_TITLE),
     ('authors', AUTHORS),
     ('html_authors', HTML_AUTHORS),
+    ('plain_authors', PLAIN_AUTHORS),
     ('abstract', ABSTRACT),
     ('html_abstract', HTML_ABSTRACT),
+    ('plain_abstract', PLAIN_ABSTRACT),
     ('is_draft', IS_DRAFT),
     ('handle', HANDLE),
     ('series', SERIES),
