e10b58459df54c13813a0b70164cf523304e85d6
Jonathan Sick <jsick@lsst.org>
2017-09-16 11:22:02 -0700

    Add DMTR-31 test sample
    No parsing changes were needed to support a DMTR. Things are getting
    robust?

---
+++ tests/test_lsstdoc_dmtr_31.py
@@ -0,0 +1,88 @@
+"""Test LsstDoc using sample data from DMTR-31.tex.
+"""
+
+import os
+import pytest
+from metasrc.tex.lsstdoc import LsstDoc
+
+TITLE = "S17B HSC PDR1 Reprocessing Report"
+
+SHORT_TITLE = None
+
+HTML_TITLE = "S17B HSC PDR1 Reprocessing Report\n"
+
+HTML_SHORT_TITLE = None
+
+PLAIN_TITLE = "S17B HSC PDR1 Reprocessing Report\n"
+
+PLAIN_SHORT_TITLE = None
+
+AUTHORS = [
+    "Hsin-Fang Chiang", "Greg Daues", "Samantha Thrush", "the NCSA team"
+]
+
+HTML_AUTHORS = [
+    "Hsin-Fang Chiang\n", "Greg Daues\n", "Samantha Thrush\n",
+    "the NCSA team\n"
+]
+
+PLAIN_AUTHORS = [
+    "Hsin-Fang Chiang\n", "Greg Daues\n", "Samantha Thrush\n",
+    "the NCSA team\n"
+]
+
+ABSTRACT = (
+    "This document captures information about the large scale HSC "
+    "reprocessing we performed in Cycle S17B."
+)
+
+HTML_ABSTRACT = (
+    "<p>This document captures information about the large scale HSC "
+    "reprocessing we performed in Cycle S17B.</p>\n"
+)
+
+PLAIN_ABSTRACT = (
+    "This document captures information about the large scale HSC "
+    "reprocessing we performed in Cycle S17B.\n"
+)
+
+IS_DRAFT = False
+
+HANDLE = 'DMTR-31'
+
+SERIES = 'DMTR'
+
+SERIAL = '31'
+
+ATTRIBUTES = [
+    ('title', TITLE),
+    ('short_title', SHORT_TITLE),
+    ('html_title', HTML_TITLE),
+    ('html_short_title', HTML_SHORT_TITLE),
+    ('plain_title', PLAIN_TITLE),
+    ('plain_short_title', PLAIN_SHORT_TITLE),
+    ('authors', AUTHORS),
+    ('html_authors', HTML_AUTHORS),
+    ('plain_authors', PLAIN_AUTHORS),
+    ('abstract', ABSTRACT),
+    ('html_abstract', HTML_ABSTRACT),
+    ('plain_abstract', PLAIN_ABSTRACT),
+    ('is_draft', IS_DRAFT),
+    ('handle', HANDLE),
+    ('series', SERIES),
+    ('serial', SERIAL),
+]
+
+
+@pytest.fixture
+def lsstdoc():
+    tex_path = os.path.join(os.path.dirname(__file__),
+                            'data',
+                            'DMTR-31',
+                            'DMTR-31.tex')
+    return LsstDoc.read(tex_path)
+
+
+@pytest.mark.parametrize('attribute,expected', ATTRIBUTES)
+def test_attribute(lsstdoc, attribute, expected):
+    assert getattr(lsstdoc, attribute) == expected
