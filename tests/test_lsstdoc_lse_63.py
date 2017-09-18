ff7325b9249cf68af9a5ac255b621168076e5ff7
Jonathan Sick <jsick@lsst.org>
2017-09-18 15:43:24 -0700

    Add LSE-63.tex test dataset
    LSE-63 includes citations in the abstract, which doesn't happen in other
    test datasets.

---
+++ tests/test_lsstdoc_lse_63.py
@@ -0,0 +1,149 @@
+"""Test LsstDoc using sample data from LSE-63.tex.
+"""
+
+import os
+import pytest
+from metasrc.tex.lsstdoc import LsstDoc
+
+TITLE = "LSST Data Quality Assurance Plan"
+
+SHORT_TITLE = None
+
+HTML_TITLE = "LSST Data Quality Assurance Plan\n"
+
+HTML_SHORT_TITLE = None
+
+PLAIN_TITLE = "LSST Data Quality Assurance Plan\n"
+
+PLAIN_SHORT_TITLE = None
+
+AUTHORS = [
+    "Tony Tyson", "DQA Team", "Science Collaboration"
+]
+
+HTML_AUTHORS = [
+    "Tony Tyson\n", "DQA Team\n", "Science Collaboration\n"
+]
+
+PLAIN_AUTHORS = [
+    "Tony Tyson\n", "DQA Team\n", "Science Collaboration\n"
+]
+
+ABSTRACT = (
+    "LSST must supply trusted petascale data products. The mechanisms by "
+    "which the LSST project achieve this unprecedented level of data "
+    "quality will have spinoff to data-enabled science generally. This "
+    "document specifies high-level requirements for a LSST Data Quality "
+    "Assessment\nFramework, and defines the four levels of quality "
+    "assessment (QA) tools. Because this process involves system-wide "
+    "hardware and software, data QA must be defined at the System level. The "
+    "scope of this document is limited to the description of the overall "
+    "framework and the general requirements. It derives from the LSST "
+    "Science Requirements Document \citedsp{LPM-17}. A flow-down document "
+    "will describe detailed implementation of the QA, including the "
+    "algorithms.  In most cases the monitoring strategy, the development "
+    "path for these tools or the algorithms are known. Related documents "
+    "are: LSST System Requirements \citedsp{LSE-29}, Optimal Deployment "
+    "Parameters \citeds{Document-11624}, Observatory System Specifications "
+    "\citedsp{LSE-30}, Configuration Management Plan \citedsp{LPM-19}, "
+    "Project Quality Assurance Plan \citedsp{LPM-55}, Software Development "
+    "Plan \citedsp{LSE-16}, Camera Quality implementation Plan "
+    "\citedsp{LCA-227}, System Engineering Management Plan \citedsp{LSE-17}, "
+    "and the Operations Plan \citedsp{LPM-73}."
+)
+
+HTML_ABSTRACT = (
+    "<p>LSST must supply trusted petascale data products. The mechanisms by "
+    "which the LSST project achieve this unprecedented level of data "
+    "quality will have spinoff to data-enabled science generally. This "
+    "document specifies high-level requirements for a LSST Data Quality "
+    "Assessment Framework, and defines the four levels of quality "
+    "assessment (QA) tools. Because this process involves system-wide "
+    "hardware and software, data QA must be defined at the System level. The "
+    "scope of this document is limited to the description of the overall "
+    "framework and the general requirements. It derives from the LSST "
+    "Science Requirements Document [<a href=\"https://ls.st/LPM-17\">"
+    "LPM-17</a>]. A flow-down document will describe detailed implementation "
+    "of the QA, including the algorithms. In most cases the monitoring "
+    "strategy, the development path for these tools or the algorithms are "
+    "known. Related documents are: LSST System Requirements "
+    "[<a href=\"https://ls.st/LSE-29\">LSE-29</a>], Optimal Deployment "
+    "Parameters <a href=\"https://ls.st/Document-11624\">Document-11624</a>, "
+    "Observatory System Specifications "
+    "[<a href=\"https://ls.st/LSE-30\">LSE-30</a>], Configuration Management "
+    "Plan [<a href=\"https://ls.st/LPM-19\">LPM-19</a>], "
+    "Project Quality Assurance Plan "
+    "[<a href=\"https://ls.st/LPM-55\">LPM-55</a>], "
+    "Software Development Plan [<a href=\"https://ls.st/LSE-16\">LSE-16</a>], "
+    "Camera Quality implementation Plan "
+    "[<a href=\"https://ls.st/LCA-227\">LCA-227</a>], "
+    "System Engineering Management Plan "
+    "[<a href=\"https://ls.st/LSE-17\">LSE-17</a>], "
+    "and the Operations Plan "
+    "[<a href=\"https://ls.st/LPM-73\">LPM-73</a>].</p>\n"
+)
+
+PLAIN_ABSTRACT = (
+    "LSST must supply trusted petascale data products. The mechanisms by "
+    "which the LSST project achieve this unprecedented level of data "
+    "quality will have spinoff to data-enabled science generally. This "
+    "document specifies high-level requirements for a LSST Data Quality "
+    "Assessment Framework, and defines the four levels of quality "
+    "assessment (QA) tools. Because this process involves system-wide "
+    "hardware and software, data QA must be defined at the System level. The "
+    "scope of this document is limited to the description of the overall "
+    "framework and the general requirements. It derives from the LSST "
+    "Science Requirements Document [LPM-17]. A flow-down document will "
+    "describe detailed implementation of the QA, including the algorithms. "
+    "In most cases the monitoring strategy, the development path for these "
+    "tools or the algorithms are known. Related documents are: "
+    "LSST System Requirements [LSE-29], "
+    "Optimal Deployment Parameters Document-11624, "
+    "Observatory System Specifications [LSE-30], "
+    "Configuration Management Plan [LPM-19], "
+    "Project Quality Assurance Plan [LPM-55], "
+    "Software Development Plan [LSE-16], "
+    "Camera Quality implementation Plan [LCA-227], "
+    "System Engineering Management Plan [LSE-17], "
+    "and the Operations Plan [LPM-73].\n"
+)
+
+IS_DRAFT = False
+
+HANDLE = 'LSE-63'
+
+SERIES = 'LSE'
+
+SERIAL = '63'
+
+ATTRIBUTES = [
+    ('title', TITLE),
+    ('short_title', SHORT_TITLE),
+    ('html_title', HTML_TITLE),
+    ('html_short_title', HTML_SHORT_TITLE),
+    ('plain_title', PLAIN_TITLE),
+    ('plain_short_title', PLAIN_SHORT_TITLE),
+    ('authors', AUTHORS),
+    ('html_authors', HTML_AUTHORS),
+    ('plain_authors', PLAIN_AUTHORS),
+    ('abstract', ABSTRACT),
+    ('html_abstract', HTML_ABSTRACT),
+    ('plain_abstract', PLAIN_ABSTRACT),
+    ('is_draft', IS_DRAFT),
+    ('handle', HANDLE),
+    ('series', SERIES),
+    ('serial', SERIAL),
+]
+
+
+@pytest.fixture
+def lsstdoc():
+    tex_path = os.path.join(os.path.dirname(__file__),
+                            'data',
+                            'LSE-63.tex')
+    return LsstDoc.read(tex_path)
+
+
+@pytest.mark.parametrize('attribute,expected', ATTRIBUTES)
+def test_attribute(lsstdoc, attribute, expected):
+    assert getattr(lsstdoc, attribute) == expected
