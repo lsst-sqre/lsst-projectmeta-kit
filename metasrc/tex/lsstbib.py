76156cfc81b91067c4014933154b0ce42c2161b5
Jonathan Sick <jsick@lsst.org>
2017-09-19 17:36:08 -0700

    Add get_bibliography function using pybtex
    get_bibliography() combines lsst-texmf bibliographies (using
    get_lsst_bibtex()) and local bibliography data to make a
    BibliographyData instance with the pybtex package. BibliographyData is a
    container class for parsed bibliography entries.
    
    Adds pybtex as a dependency.

--- metasrc/tex/lsstbib.py
+++ metasrc/tex/lsstbib.py
@@ -1,12 +1,14 @@
 """APIs for getting and working with the BibTeX databases in lsst-texmf.
 """
 
-__all__ = ['get_lsst_bibtex']
+__all__ = ['get_lsst_bibtex', 'get_bibliography']
 
 import asyncio
 import logging
+import os
 
 from aiohttp import ClientSession
+import pybtex.database
 
 # https://lsst-texmf.lsst.io/lsstdoc.html#bibliographies
 KNOWN_LSSTTEXMF_BIB_NAMES = ('lsst', 'lsst-dm', 'refs', 'books', 'refs_ads')
@@ -125,3 +127,48 @@ def get_lsst_bibtex(bibtex_filenames=None):
             _LSSTTEXMF_BIB_CACHE[name] = text
 
     return {name: _LSSTTEXMF_BIB_CACHE[name] for name in bibtex_names}
+
+
+def get_bibliography(lsst_bib_names=None, bibtex=None):
+    """Make a pybtex BibliographyData instance from standard lsst-texmf
+    bibliography files and user-supplied bibtex content.
+
+    Parameters
+    ----------
+    lsst_bib_names : sequence of `str`, optional
+        Names of lsst-texmf BibTeX files to include. For example:
+
+        .. code-block:: python
+
+           ['lsst', 'lsst-dm', 'refs', 'books', 'refs_ads']
+
+        Default is `None`, which includes all lsst-texmf bibtex files.
+
+    bibtex : `str`
+        BibTeX source content not included in lsst-texmf. This can be content
+        from a import ``local.bib`` file.
+
+    Returns
+    -------
+    bibliography : `pybtex.database.BibliographyData`
+        A pybtex bibliography database that includes all given sources:
+        lsst-texmf bibliographies and ``bibtex``.
+    """
+    bibtex_data = get_lsst_bibtex(bibtex_filenames=lsst_bib_names)
+
+    # Parse with pybtex into BibliographyData instances
+    pybtex_data = [pybtex.database.parse_string(_bibtex, 'bibtex')
+                   for _bibtex in bibtex_data.values()]
+
+    # Also parse local bibtex content
+    if bibtex is not None:
+        pybtex_data.append(pybtex.database.parse_string(bibtex, 'bibtex'))
+
+    # Merge BibliographyData
+    bib = pybtex_data[0]
+    if len(pybtex_data) > 1:
+        for other_bib in pybtex_data[1:]:
+            for key, entry in other_bib.entries.items():
+                bib.add_entry(key, entry)
+
+    return bib
--- tests/test_lsstbib.py
+++ tests/test_lsstbib.py
@@ -1,7 +1,9 @@
 """Tests for the metasrc.tex.lsstbib module.
 """
 
-from metasrc.tex.lsstbib import get_lsst_bibtex
+from pybtex.database import BibliographyData
+
+from metasrc.tex.lsstbib import get_lsst_bibtex, get_bibliography
 
 
 def test_get_lsst_bibtex():
@@ -19,3 +21,19 @@ def test_get_lsst_bibtex():
     assert 'refs' in bibtex
     assert 'books' in bibtex
     assert 'refs_ads' in bibtex
+
+
+def test_get_bibliography():
+    bib = get_bibliography()
+    assert isinstance(bib, BibliographyData)
+
+    # Key from 'lsst' bibtex file
+    assert 'LDM-151' in bib.entries
+    # Key from 'lsst-dm' bibtex file
+    assert '2002SPIE.4844..225C' in bib.entries
+    # Key from 'refs' bibtex file
+    assert '1991.Spyak.OpticalEngineering' in bib.entries
+    # Key from 'books' bibtex file
+    assert 'Adass08' in bib.entries
+    # Key from 'refs_ads' bibtex file
+    assert '1727RSPT...35..637B' in bib.entries
