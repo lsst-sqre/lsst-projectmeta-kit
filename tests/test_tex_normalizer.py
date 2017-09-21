da68e6f003634ffd6214331aa173cde73c9db483
Jonathan Sick <jsick@lsst.org>
2017-09-21 10:26:41 -0700

    Rename texnormalizer module to normalizer
    It's already in the tex sub-package, so this name makes sense.

--- tests/test_tex_normalizer.py
+++ tests/test_tex_normalizer.py
@@ -1,4 +1,4 @@
-"""Tests for the metasrc.tex.texnormalizer module.
+"""Tests for the metasrc.tex.normalizer module.
 """
 
 import os
@@ -6,7 +6,7 @@ import re
 
 import pytest
 
-import metasrc.tex.texnormalizer as texnormalizer
+import metasrc.tex.normalizer as normalizer
 
 
 def test_remove_comments_abstract():
@@ -31,26 +31,26 @@ def test_remove_comments_abstract():
         " environments as well as the interfaces among them, the rest\n"
         " of the LSST system, and the outside world.\n"
         "}")
-    assert texnormalizer.remove_comments(sample) == expected
+    assert normalizer.remove_comments(sample) == expected
 
 
 def test_escaped_remove_comments():
     """Test remove_comments where a "%" is escaped."""
     sample = "The uncertainty is 5\%.  % a comment"
     expected = "The uncertainty is 5\%.  "
-    assert texnormalizer.remove_comments(sample) == expected
+    assert normalizer.remove_comments(sample) == expected
 
 
 def test_single_line_remove_comments():
     sample = "This is content.  % a comment"
     expected = "This is content.  "
-    assert texnormalizer.remove_comments(sample) == expected
+    assert normalizer.remove_comments(sample) == expected
 
 
 def test_remove_single_line_trailing_whitespace():
     sample = "This is content.    "
     expected = "This is content."
-    assert texnormalizer.remove_trailing_whitespace(sample) == expected
+    assert normalizer.remove_trailing_whitespace(sample) == expected
 
 
 def test_multi_line_trailing_whitespace():
@@ -58,13 +58,13 @@ def test_multi_line_trailing_whitespace():
               "Second line. ")
     expected = ("First line.\n"
                 "Second line.")
-    assert texnormalizer.remove_trailing_whitespace(sample) == expected
+    assert normalizer.remove_trailing_whitespace(sample) == expected
 
 
 def test_read_tex_file():
     project_dir = os.path.join(os.path.dirname(__file__), 'data', 'texinputs')
     root_filepath = os.path.join(project_dir, 'LDM-nnn.tex')
-    tex_source = texnormalizer.read_tex_file(root_filepath)
+    tex_source = normalizer.read_tex_file(root_filepath)
 
     # verify that input'd and include'd content is present
     assert re.search(r'\\setDocAbstract', tex_source) is not None
@@ -85,7 +85,7 @@ def test_replace_macros():
         r"This is the  Test Plan for Data Management.}")
 
     macros = {r'\product': 'Data Management'}
-    tex_source = texnormalizer.replace_macros(sample, macros)
+    tex_source = normalizer.replace_macros(sample, macros)
     assert re.search(r'\\product', sample) is not None  # sanity check
     assert re.search(r'\\product', tex_source) is None
     assert tex_source == expected
@@ -106,5 +106,5 @@ def test_replace_macros():
      ('\include file\n', 'file'),
      ('\include file \n', 'file')])
 def test_input_include_pattern(sample, expected):
-    match = re.search(texnormalizer.input_include_pattern, sample)
+    match = re.search(normalizer.input_include_pattern, sample)
     assert match.group('filename') == expected
