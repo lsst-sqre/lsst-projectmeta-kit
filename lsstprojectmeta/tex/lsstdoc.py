bb854b4714e1078cf1acc0235c1a897735b944ac
Jonathan Sick <jsick@lsst.org>
2018-01-22 15:10:28 -0700

    Add plain text content to JSON articleBody
    - Using plain text lets use use the tex document in search applications.
    - The fileFormat field indicates the format of articleBody.

--- lsstprojectmeta/tex/lsstdoc.py
+++ lsstprojectmeta/tex/lsstdoc.py
@@ -73,6 +73,11 @@ class LsstLatexDoc(object):
         return cls(tex_source, root_dir=root_dir)
 
     @property
+    def plain_content(self):
+        """Plain-text-formatted document content (`str`)."""
+        return self.format_content(format='plain', mathjax=False, smart=True)
+
+    @property
     def html_title(self):
         """HTML5-formatted document title (`str`)."""
         return self.format_title(format='html5', deparagraph=True,
@@ -245,6 +250,34 @@ class LsstLatexDoc(object):
             # Load reference BibTeX into a pybtex BibliographyData
             self._load_bib_db()
         return self._bib_db
+
+    def format_content(self, format='plain', mathjax=False,
+                       smart=True, extra_args=None):
+        """Get the document content in the specified markup format.
+
+        Parameters
+        ----------
+        format : `str`, optional
+            Output format (such as ``'html5'`` or ``'plain'``).
+        mathjax : `bool`, optional
+            Allow pandoc to use MathJax math markup.
+        smart : `True`, optional
+            Allow pandoc to create "smart" unicode punctuation.
+        extra_args : `list`, optional
+            Additional command line flags to pass to Pandoc. See
+            `lsstprojectmeta.pandoc.convert.convert_text`.
+
+        Returns
+        -------
+        output_text : `str`
+            Converted content.
+        """
+        output_text = convert_lsstdoc_tex(
+            self._tex, format,
+            mathjax=mathjax,
+            smart=smart,
+            extra_args=extra_args)
+        return output_text
 
     def format_title(self, format='html5', deparagraph=True, mathjax=False,
                      smart=True, extra_args=None):
@@ -697,6 +730,8 @@ class LsstLatexDoc(object):
             'reportNumber': self.handle,
             'name': self.plain_title,
             'description': self.plain_abstract,
+            'articleBody': self.plain_content,
+            'fileFormat': 'text/plain',  # MIME type of articleBody
             'author': [{'@type': 'Person', 'name': author_name}
                        for author_name in self.plain_authors],
             # This is a datetime.datetime; not a string. If writing to a file,
